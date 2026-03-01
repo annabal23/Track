0840ca3fef29e809ef2064be4a8a8f0976e8f3c8
annabal23 <annabal23@yandex.ru>
2026-03-01 22:47:54 +0300

    Лабораторна№3_task1.py

--- Лабораторна№3_task1.py
+++ Лабораторна№3_task1.py
@@ -43,13 +43,34 @@ class PaperBook:
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
+        
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
 
-class AudioBook:
+class AudioBook(Book):
+    """ Аудиокнига. """
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self.duration = duration  # Используем сеттер для проверки
+
+    @property
+    def duration(self) -> float:
+        """Геттер для продолжительности."""
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: float):
+        """Сеттер для продолжительности с проверкой."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("Продолжительность должна быть числом")
+        if value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = float(value)
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        # Переопределяем __str__ чтобы включить информацию о длительности
+        return f"Аудиокнига {self.name}. Автор {self.author}. Длительность: {self.duration} часов"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
